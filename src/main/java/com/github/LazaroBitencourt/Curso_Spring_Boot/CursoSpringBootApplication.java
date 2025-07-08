1773317bfcb30d4b59b495ac9c15231a92ce5005
Lázaro Bitencourt <lazaro_boc@yahoo.com.br>
2025-07-08 00:27:32 -0300

    first commit

---
+++ src/main/java/com/github/LazaroBitencourt/Curso_Spring_Boot/CursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.LazaroBitencourt.Curso_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+}
